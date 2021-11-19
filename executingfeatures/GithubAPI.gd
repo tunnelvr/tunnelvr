47b34909f5465ff056c35259b17de951dc345bfd
Julian Todd <julian@goatchurch.org.uk>
2021-11-19 21:55:09 +0000

    githubapi for fetch and commit cave files added

---
+++ executingfeatures/GithubAPI.gd
@@ -0,0 +1,120 @@
+extends Node
+
+var ghdirectory = "user://githubcache"
+var ghattributes = null  # {"apiurl":"api.github.com", "owner":"goatchurchprime", "repo":"abdulsdiodedisaster", "path":"abdulsdiodedisaster", "token":"see https://github.com/settings/tokens"}
+var ghcurrentname = ""
+var ghcurrentsha = ""
+var ghfetcheddatafile = ghdirectory+"/recgithubfile.res"
+var httpghapi = HTTPClient.new()
+
+func Yinitclient():
+	if ghattributes == null:
+		var dir = Directory.new()
+		if not dir.dir_exists(ghdirectory):
+			dir.make_dir(ghdirectory)
+
+		var ghjson = File.new()
+		ghjson.open(ghdirectory+"/attributes.json", File.READ)
+		ghattributes = parse_json(ghjson.get_as_text())
+		ghjson.close()
+		if ghattributes == null:
+			ghattributes = '{"apiurl":"api.github.com", "owner":"goatchurchprime", "repo":"abdulsdiodedisaster", "path":"abdulsdiodedisaster", "token":"see https://github.com/settings/tokens"}'
+
+	yield(Engine.get_main_loop(), "idle_frame")
+	if httpghapi.get_status() != HTTPClient.STATUS_CONNECTED:
+		var e = httpghapi.connect_to_host(ghattributes["apiurl"], -1, true)
+		print(e, " status ", httpghapi.get_status())
+		while httpghapi.get_status() == HTTPClient.STATUS_CONNECTING or httpghapi.get_status() == HTTPClient.STATUS_RESOLVING:
+			httpghapi.poll()
+			yield(Engine.get_main_loop(), "idle_frame")
+			print(httpghapi.get_status())
+
+func Yghapicall(method, rpath, body):
+	yield(Yinitclient(), "completed")
+	var http = httpghapi
+	var headers = [ "User-Agent: TunnelVR/0.7 (Godot)", 
+					"Accept: application/vnd.github.v3+json", 
+					"Authorization: token "+ghattributes["token"] ]
+	var err = http.request(method, rpath, headers, body) 
+	if err != OK:
+		return null
+	print(" httpstatus ", http.get_status())
+	while http.get_status() == HTTPClient.STATUS_REQUESTING:
+		http.poll()
+		yield(Engine.get_main_loop(), "idle_frame")
+	if not http.has_response() or http.get_response_code() != 200 or http.get_status() != HTTPClient.STATUS_BODY:
+		print("bad response code: ", http.get_response_code())
+		return null
+	var rb = PoolByteArray()
+	while http.get_status() == HTTPClient.STATUS_BODY:
+		http.poll()
+		var chunk = http.read_response_body_chunk()
+		if chunk.size() == 0:
+			yield(Engine.get_main_loop(), "idle_frame")
+		else:
+			rb = rb + chunk
+	var rt = rb.get_string_from_ascii()
+	return parse_json(rt)
+
+func Ylistghfiles():
+	yield(Yinitclient(), "completed")
+	var rpath = "/repos/%s/%s/contents/%s" % [ ghattributes["owner"], ghattributes["repo"], ghattributes["path"] ]
+	var d = yield(Yghapicall(HTTPClient.METHOD_GET, rpath, ""), "completed")
+	var cfiles = [ ]
+	for k in d:
+		if k["type"] == "file":
+			cfiles.push_back(k["name"])
+	return cfiles
+
+func Yfetchfile(cname):
+	yield(Yinitclient(), "completed")
+	var rpath = "/repos/%s/%s/contents/%s/%s" % [ ghattributes["owner"], ghattributes["repo"], ghattributes["path"], cname ]
+	var d = yield(Yghapicall(HTTPClient.METHOD_GET, rpath, ""), "completed")
+	if d.get("type") != "file" or d["encoding"] != "base64":
+		return null
+	ghcurrentname = d["name"]
+	ghcurrentsha = d["sha"]
+	var f = File.new()
+	f.open(ghfetcheddatafile, File.WRITE)
+	f.store_buffer(Marshalls.base64_to_raw(d["content"]))
+	f.close()
+	return ghfetcheddatafile
+
+func Ycommitfile(cname, message):
+	yield(Yinitclient(), "completed")
+	var f = File.new()
+	f.open(ghfetcheddatafile, File.READ)
+	var contents = f.get_buffer(f.get_len())
+	f.close()
+	var rpath = "/repos/%s/%s/contents/%s/%s" % [ ghattributes["owner"], ghattributes["repo"], ghattributes["path"], cname ]
+	var put_parameters = { "message":message, "content":Marshalls.raw_to_base64(contents) }
+	if cname == ghcurrentname:
+		put_parameters["sha"] = ghcurrentsha
+	var d = yield(Yghapicall(HTTPClient.METHOD_PUT, rpath, to_json(put_parameters)), "completed")
+	if d == null or not d.has("content") or d["content"].get("type") != "file":
+		return null
+	ghcurrentname = d["content"]["name"]
+	ghcurrentsha = d["content"]["sha"]
+	return ghfetcheddatafile
+
+
+func addstufftofile():
+	var ghrawfile = File.new()
+	ghrawfile.open(ghfetcheddatafile, File.READ_WRITE)
+	var h = ghrawfile.get_buffer(ghrawfile.get_len())
+	ghrawfile.store_buffer(h)
+	ghrawfile.store_buffer("\nding ding!\n".to_ascii())
+	ghrawfile.close()
+var message = "saywhat"
+func D_input(event):	
+	if event is InputEventKey and event.pressed and event.scancode == KEY_8:
+		print(yield(Ylistghfiles(), "completed"))
+		
+	var ghfetchedname = "madebyapi.txt"
+	if event is InputEventKey and event.pressed and event.scancode == KEY_9:
+		print(yield(Yfetchfile("madebyapi.txt"), "completed"))
+		
+	if event is InputEventKey and event.pressed and event.scancode == KEY_K:
+		addstufftofile()
+		message = message + "-"
+		print(yield(Ycommitfile("madebyapi.txt", message), "completed"))
--- immediate_code.gd
+++ immediate_code.gd
@@ -6,23 +6,75 @@ extends EditorScript
 # *******
 
 
-func thing(x):
-	var v = Timer.new()
-	v.one_shot = true
-	v.wait_time = 3.0
-	v.start()
-	#yield(v, "timeout")
-	print("kkkkk ", x)
-
-signal sthing(x)
-
 func _run():
-	var t = Transform2D()
-	t = t.translated(Vector2(8, 9))
-	t = t.scaled(Vector2(0.05, 1))
-	print(t, t.affine_inverse())
-	var p = Vector2(10, 20)
-	var q = t.xform(p)
-	var p1 = t.affine_inverse().xform(q)
-	print(p, q, p1)
+	var h = "https://raw.githubusercontent.com/goatchurchprime/abdulsdiodedisaster/master/junk/madebyapi.txt"
+	var b = h.split("/", true, 3)
+	print(b)
+	assert(b[0] == "https:" and b[1] == "")
+	print(b[2])
+	print(b[3])
+	print(null or PoolByteArray())
+		
+func DD_run():
+
+	var http = HTTPClient.new()
+	var e = http.connect_to_host("api.github.com", -1, true)
+#https://github.com/goatchurchprime/abdulsdiodedisaster/blob/master/junk/madebyapi.txt
+	print(e, " ", http.get_status())
+	
+	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
+		http.poll()
+		yield(Engine.get_main_loop(), "idle_frame")
+		print(http.get_status())
+
+	var headers = [
+		"User-Agent: Pirulo/1.0 (Godot)",
+		"Accept: */*"
+	]
+
+	var err = http.request(HTTPClient.METHOD_GET, "/repos/goatchurchprime/abdulsdiodedisaster/contents/junk", headers) # Request a page from the site (this one was chunked..)
+	assert(err == OK) # Make sure all is OK.
+
+	while http.get_status() == HTTPClient.STATUS_REQUESTING:
+		http.poll()
+		print("Requesting...")
+		yield(Engine.get_main_loop(), "idle_frame")
+
+	assert(http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED) # Make sure request finished well.
+
+	print("response? ", http.has_response()) # Site might not have a response.
+	if http.has_response():
+		headers = http.get_response_headers_as_dictionary() # Get response headers.
+		print("code: ", http.get_response_code()) # Show response code.
+		print("**headers:\\n", headers) # Show headers.
+
+		# Getting the HTTP Body
+
+		if http.is_response_chunked():
+			print("Response is Chunked!")
+		else:
+			var bl = http.get_response_body_length()
+			print("Response Length: ", bl)
+
+		var rb = PoolByteArray() # Array that will hold the data.
+		while http.get_status() == HTTPClient.STATUS_BODY:
+			http.poll()
+			# Get a chunk.
+			var chunk = http.read_response_body_chunk()
+			if chunk.size() == 0:
+				yield(Engine.get_main_loop(), "idle_frae")
+			else:
+				rb = rb + chunk # Append to read buffer.
+
+		print("bytes got: ", rb.size())
+		var text = rb.get_string_from_ascii()
+		print("Text: ", text)
+		
+		var d = parse_json(text)
+		print(d)
+		print(http.connection)
+		
+	
+#		curl https://api.github.com/repos/goatchurchprime/abdulsdiodedisaster/contents/junk
+#		/repos/{owner}/{repo}/contents/{path} <http://docs.github.com/en/rest/reference/repos#contents>`_
 	
