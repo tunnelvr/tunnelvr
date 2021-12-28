8ff8289e419645b7ada023370f0bb0ee863e8cca
Julian Todd <julian@goatchurch.org.uk>
2021-12-28 16:47:13 +0000

    small hack to overcome xml_unescape bug 56298 in godot

--- immediate_code.gd
+++ immediate_code.gd
@@ -6,7 +6,7 @@ extends EditorScript
 # *******
 
 
-func _run():
+func D_run():
 
 	var a = Vector3(0,0,0)
 	var b = Vector3(2,0,0)
@@ -16,3 +16,15 @@ func _run():
 	for i in range(11):
 		var weight = i/10.0
 		print(a.cubic_interpolate(b, pre_a, post_b, weight))
+func _run():
+	var x = "&#21E7;"
+	if x[0] == "&":
+		print(("&#%d;" % ("0x"+x.substr(2, 4)).hex_to_int()).xml_unescape())
+	
+	print("&#21E7;".xml_unescape())
+	print("sssss &#0059;".xml_unescape())
+	
+	print("&".xml_escape())
+	print("&lt;".xml_unescape())
+	print("0x232B".hex_to_int())
+	
